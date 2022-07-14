5c07c3581f762fceb3312343b961946038e3cec4
Geoffrey House <geo.house7@gmail.com>
2022-07-13 18:42:39 -0600

    update to see how params are initialized

--- js/hoistingExp.js
+++ js/hoistingExp.js
@@ -19,12 +19,15 @@ function getName() {
 
 let getName2 = function () {
   console.log("getName2");
-  function getNameNested() {
+  // the variable `input` gets passed as a parameter WITH a value immediately upon function invocation (i.e. during the memory creation
+  // phase when `x` is still undefined, `input` = "testInput" from the argument of its call.)
+  function getNameNested(input) {
+    let x = 6;
     console.log("getNameNested");
   }
   // When the getNameNested function runs, because it's nested, it's on the top of the call stack, with getName2 below it and the
-  // the global execution context below that.
-  getNameNested();
+  // the global execution context below that (see with debugging then looking at the call stack).
+  getNameNested("testInput");
 };
 
 let getName3 = () => {
